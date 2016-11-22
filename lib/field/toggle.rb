1af8e071dd01d7f386824f392c2b861e3cea9170
Brandon <d2sm10@hotmail.com>
2016-11-21 23:52:17 -0700

    Added new toggle field type.

---
+++ lib/field/toggle.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+module DFormed
+  class Toggle < Field
+    attr_bool :value, default: false, serialize: true, always: true
+    serialize_method :attributes, :clean_attributes, ignore: {}
+
+    after :setup_attributes, :value=, :default=
+
+    def self.type
+      :toggle
+    end
+
+    def validate
+      true
+    end
+
+    def toggle
+      value = !value
+    end
+
+    # These methods are only available if the engine is Opal
+    if DFormed.in_opal?
+
+      def retrieve_value
+        self.value = @element.is(':checked')
+      end
+
+      def to_element
+        super
+        setup_attributes
+      end
+
+    end
+
+    protected
+
+    def inner_html
+      nil
+    end
+
+    def lazy_setup
+      super
+      @tagname = 'input'
+      setup_attributes
+    end
+
+    def setup_attributes
+      add_attribute(name: name, type: :checkbox)
+      @element.prop(:checked, value) if element?
+    end
+
+    def clean_attributes
+      temp = @attributes.dup
+      temp.delete(:name)
+      temp.delete(:type)
+      temp
+    end
+
+  end
+end
