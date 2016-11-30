8db4bf10ab47328ab14b2be4f0e89b6cabbc700c
Brandon <d2sm10@hotmail.com>
2016-11-29 23:06:29 -0700

    Made jquery selectors a bit more specific.

--- lib/form/vertical_form.rb
+++ lib/form/vertical_form.rb
@@ -15,11 +15,11 @@ module DFormed
         until x >= @fields.size
           row = Element['<tr class="form_row"><td class="label"/><td class="field"/></tr>']
           if @fields[x].is_a?(Label) && @fields[x+1] && !@fields[x+1].is_a?(Label)
-            row.find('.label').append(@fields[x].to_element)
-            row.find('.field').append(@fields[x+1].to_element)
+            row.find('td.label').append(@fields[x].to_element)
+            row.find('td.field').append(@fields[x+1].to_element)
             x+=2
           elsif @fields[x].is_a?(Label)
-            row.find('.label').append(@fields[x].to_element)
+            row.find('td.label').append(@fields[x].to_element)
             x+=1
           elsif @fields[x].is_a?(Separator)
             row.empty
@@ -28,7 +28,7 @@ module DFormed
             row.append td
             x+=1
           else
-            row.find('.field').append(@fields[x].to_element)
+            row.find('td.field').append(@fields[x].to_element)
             x+=1
           end
           @element.append(row)
