c526f1e8a18ef0109c7c6b0890375da7bbb4111f
Brandon Black <d2sm10@hotmail.com>
2017-09-25 07:31:12 -0600

    Refactoring of default forms now that labels are no longer needed.

--- lib/form/vertical_form.rb
+++ lib/form/vertical_form.rb
@@ -9,41 +9,16 @@ module DFormed
     if DFormed.in_opal?
 
       def to_element
-        # @element = Element['<div/>']
-        # x = 0
-        # until x >= fields.size
-        #   row = Element['<div class="form_row">']
-        #   if fields[x].is_a?(Label) && fields[x + 1] && !fields[x + 1].is_a?(Label)
-        #     row.append(fields[x].to_element)
-        #     row.append(fields[x + 1].to_element)
-        #     x += 2
-        #   else
-        #     row.append(fields[x].to_element)
-        #   end
-        #   element.append(row)
-        # end
-        # element
-        header = "<table class='fields'></table>"
+        header = "<table class='dformed-vform fields'></table>"
         @element = Element[header]
-        x = 0
-        until x >= @fields.size
-          row = Element['<tr class="form_row"><td class="label"/><td class="field"/></tr>']
-          if @fields[x].is_a?(Label) && @fields[x+1] && !@fields[x+1].is_a?(Label)
-            row.find('td.label').append(@fields[x].to_element)
-            row.find('td.field').append(@fields[x+1].to_element)
-            x+=2
-          elsif @fields[x].is_a?(Label)
-            row.find('td.label').append(@fields[x].to_element)
-            x+=1
-          elsif @fields[x].is_a?(Separator)
-            row.empty
-            td = Element['<td colspan=2/>']
-            td.append @fields[x].to_element
-            row.append td
-            x+=1
+        fields.each do |f|
+          if f.is_a?(Field)
+            row = Element['<tr class="form-row"><td class="dformed-label"/><td class="dformed-field"></tr>']
+            row.find('.dformed-label').append("<label class='dformed-field-label'>#{f.label}</label>") if f.labeled?
+            row.find('.dformed-field').append(f.to_element)
           else
-            row.find('td.field').append(@fields[x].to_element)
-            x+=1
+            row = Element['<tr class="form-row"><td class="dformed-element"/></tr>']
+            row.find('.dformed-element').append(f.to_element)
           end
           @element.append(row)
         end
