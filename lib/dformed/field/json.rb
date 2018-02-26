2ba6dae2166922b1ecc731a63b6d06a7e32ab6e8
Brandon Black <d2sm10@hotmail.com>
2018-02-26 07:32:11 -0700

    Fixed json parsing in Opal.

--- lib/dformed/field/json.rb
+++ lib/dformed/field/json.rb
@@ -1,6 +1,28 @@
 module DFormed
   class Json < TextArea
-    # after :to_element, :value=, :parse_json if BBLib.in_opal?
+    attr_of [Hash, Array], :value, allow_nil: true, default: nil, pre_proc: proc { |x| x.is_a?(String) ? JSON.parse(x) : x }
+
+    def parse_json
+      `try { #{_parse} } catch(e) { this.$mark_failed(e.message) }`
+      nil
+    end
+
+    def to_tag
+      BBLib::HTML.build(:textarea, value.to_json, **full_attributes)
+    end
+
+    def to_element
+      super
+      parse_json
+      element
+    end
+
+    def retrieve_value(raise_errors = false)
+      return value unless element?
+      @value = JSON.parse(element.value)
+    rescue => e
+      raise_errors ? raise(e) : nil
+    end
 
     protected
 
@@ -8,22 +30,21 @@ module DFormed
       on(:change, :parse_json, type: :method)
     end
 
-    def parse_json
-      `try { #{_parse} } catch(e) { this.$mark_failed(e.message) }`
-      nil
-    end
-
     def _parse
-      val = element? ? retrieve_value : value
-      parsed = JSON.parse(val) rescue nil
-      self.value = `JSON.stringify(#{parsed.to_n}, null, 2)`
+      val = (element? ? retrieve_value(true) : value)
+      return mark_failed('Malformed or empty JSON.') unless val
+      element.val(`JSON.stringify(#{val.to_n}, null, 2)`)
       remove_attribute(:title)
+      add_class('json-parse-succeeded')
       remove_class('json-parse-failed')
+    rescue => e
+      mark_failed(e.to_s)
     end
 
     def mark_failed(error)
       add_attribute(title: error)
       add_class('json-parse-failed')
+      remove_class('json-parse-succeeded')
     end
   end
 end
